e7abcc258438dd4b2c17c87c03ce5fbfdb837c7a
Lolilycho <85595489+Lolilycho@users.noreply.github.com>
2025-09-08 01:49:52 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,7 @@ import discord
 from discord.ext import commands
 import os
 
-intents = discord.Intents.default()
-intents.message_content = True   # メッセージ内容
-intents.members = True           # メンバー操作
-
+intents = discord.Intents.all()  # ← 全部ON
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
